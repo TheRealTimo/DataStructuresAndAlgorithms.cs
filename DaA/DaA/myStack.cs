d151baecf54a16a3caf923c3a68d2a739d32d193
Timo <31155789+TheRealTimo@users.noreply.github.com>
2023-03-19 21:57:11 +0100

    Stack
    Implemented the stack and the matching unit tests which all pass

--- DaA/DaA/myStack.cs
+++ DaA/DaA/myStack.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DaA
 {
@@ -14,6 +15,18 @@ namespace DaA
         {
             Items = new List<T>();
         }
+
+        public int Count
+        {
+            get { return Items.Count; }
+        }
+
+
+        public IEnumerable<T> GetItems()
+        {
+            return Items.AsEnumerable();
+        }
+
 
         public void Push(T item)
         {
@@ -52,6 +65,132 @@ namespace DaA
             Items[index1] = Items[index2];
             Items[index2] = temp;
         }
+
+        public bool LinearSearch(T value, int startIndex, int endIndex)
+        {
+            if (startIndex < 0 || endIndex >= Items.Count || startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException("Invalid start or end index.");
+            }
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (Items[i].CompareTo(value) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool ExponentialSearch(T value, int startIndex, int endIndex)
+        {
+            if (startIndex < 0 || endIndex >= Items.Count || startIndex > endIndex)
+            {
+                throw new ArgumentOutOfRangeException("Invalid start or end index.");
+            }
+
+            if (Items[startIndex].CompareTo(value) == 0)
+            {
+                return true;
+            }
+
+            int bound = 1;
+            while (bound <= endIndex - startIndex && Items[startIndex + bound].CompareTo(value) <= 0)
+            {
+                if (Items[startIndex + bound].CompareTo(value) == 0)
+                {
+                    return true;
+                }
+                bound *= 2;
+            }
+
+            int left = Math.Max(bound / 2, startIndex);
+            int right = Math.Min(bound, endIndex);
+            return BinarySearch(value, left, right);
+        }
+
+
+        private bool BinarySearch(T value, int startIndex, int endIndex)
+        {
+            while (startIndex <= endIndex)
+            {
+                int middleIndex = (startIndex + endIndex) / 2;
+                int comparisonResult = Items[middleIndex].CompareTo(value);
+
+                if (comparisonResult == 0)
+                {
+                    return true;
+                }
+                else if (comparisonResult < 0)
+                {
+                    startIndex = middleIndex + 1;
+                }
+                else
+                {
+                    endIndex = middleIndex - 1;
+                }
+            }
+
+            return false;
+        }
+        public void BubbleSort(int startIndex, int endIndex)
+        {
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                for (int j = startIndex; j < endIndex - i + startIndex; j++)
+                {
+                    if (Items[j].CompareTo(Items[j + 1]) > 0)
+                    {
+                        Swap(j, j + 1);
+                    }
+                }
+            }
+        }
+
+        public void QuickSort(int startIndex, int endIndex)
+        {
+            QuickSortHelper(startIndex, endIndex);
+        }
+
+        private void QuickSortHelper(int startIndex, int endIndex)
+        {
+            if (startIndex >= endIndex)
+            {
+                return;
+            }
+
+            int pivotIndex = Partition(startIndex, endIndex);
+            QuickSortHelper(startIndex, pivotIndex - 1);
+            QuickSortHelper(pivotIndex + 1, endIndex);
+        }
+
+        private int Partition(int startIndex, int endIndex)
+        {
+            T pivot = Items[endIndex];
+            int i = startIndex - 1;
+
+            for (int j = startIndex; j < endIndex; j++)
+            {
+                if (Items[j].CompareTo(pivot) <= 0)
+                {
+                    i++;
+                    Swap(i, j);
+                }
+            }
+
+            Swap(i + 1, endIndex);
+            return i + 1;
+        }
+
+        private void Swap(int index1, int index2, List<T> list)
+        {
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+
 
     }
 }
--- DaA/DaATests/UnitTest.cs
+++ DaA/DaATests/UnitTest.cs
@@ -102,4 +102,108 @@ namespace DaATests
             Assert.IsFalse(list.ExponentialSearch(5, 0, 2));
         }
     }
+
+    [TestClass]
+    public class MyStackTests
+    {
+        [TestMethod]
+        public void TestLinearSearch()
+        {
+            var stack = new myStack<int>();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(9);
+            stack.Push(1);
+            stack.Push(8);
+            stack.Push(3);
+
+            Assert.IsTrue(stack.LinearSearch(9, 0, stack.Count - 1));
+            Assert.IsFalse(stack.LinearSearch(10, 0, stack.Count - 1));
+            Assert.IsTrue(stack.LinearSearch(8, 3, 5));
+            Assert.IsFalse(stack.LinearSearch(8, 0, 2));
+        }
+
+
+
+        [TestMethod]
+        public void TestExponentialSearch()
+        {
+            var stack = new myStack<int>();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(9);
+            stack.Push(1);
+            stack.Push(8);
+            stack.Push(3);
+
+            Assert.IsTrue(stack.ExponentialSearch(5, 0, stack.Count - 1));
+            Assert.IsFalse(stack.ExponentialSearch(4, 0, stack.Count - 1));
+            Assert.IsTrue(stack.ExponentialSearch(8, 3, 5));
+            Assert.IsFalse(stack.ExponentialSearch(8, 0, 2));
+        }
+
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            var stack = new myStack<int>();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(9);
+            stack.Push(1);
+            stack.Push(8);
+            stack.Push(3);
+
+            stack.BubbleSort(0, stack.Count -1);
+
+            Assert.AreEqual("1, 2, 3, 5, 8, 9", string.Join(", ", stack.GetItems()));
+        }
+
+        [TestMethod]
+        public void TestBubbleSortPartialRange()
+        {
+            var stack = new myStack<int>();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(9);
+            stack.Push(1);
+            stack.Push(8);
+            stack.Push(3);
+
+            stack.BubbleSort(1, stack.Count - 1);
+
+            Assert.AreEqual("5, 1, 2, 3, 8, 9", string.Join(", ", stack.GetItems()));
+        }
+
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            var stack = new myStack<int>();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(9);
+            stack.Push(1);
+            stack.Push(8);
+            stack.Push(3);
+
+            stack.QuickSort(0, stack.Count -1);
+
+            Assert.AreEqual("1, 2, 3, 5, 8, 9", string.Join(", ", stack.GetItems()));
+        }
+
+        [TestMethod]
+        public void TestQuickSortPartialRange()
+        {
+            var stack = new myStack<int>();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(9);
+            stack.Push(1);
+            stack.Push(8);
+            stack.Push(3);
+
+            stack.QuickSort(1, stack.Count - 1);
+
+            Assert.AreEqual("5, 1, 2, 3, 8, 9", string.Join(", ", stack.GetItems()));
+        }
+    }
 }
